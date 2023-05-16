ca296c53bb2da9559217567b9e1703b2c0819f3c
Beztix <sebastian.luetzow@gmail.com>
2023-05-16 15:11:45 +0200

    improve wall tileset, add office walls

---
+++ tsx/Japan-Office2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Japan-Office2" tilewidth="32" tileheight="32" tilecount="2262" columns="78">
+ <image source="../tilesets/The Japan Collection Office Interior Game Assets/Tilemap/MainTileMap2.png" width="2496" height="928"/>
+</tileset>
