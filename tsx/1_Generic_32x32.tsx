94541c63ed42b440cd164a8f7de34855b6ce7169
Beztix <sebastian.luetzow@gmail.com>
2023-05-22 17:12:44 +0200

    restructure tilemaps, add details to office

---
+++ tsx/1_Generic_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="1_Generic_32x32" tilewidth="32" tileheight="32" tilecount="960" columns="16">
+ <image source="../tilesets/Modern Interior (old)/1_Generic_32x32.png" width="512" height="1920"/>
+</tileset>
---
+++ tsx/stuff_animations.tsx
@@ -0,0 +1,92 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="stuff_animations" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../tilesets/Neuroforge custom/stuff_animations.png" width="1024" height="1024"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="66" duration="200"/>
+   <frame tileid="128" duration="200"/>
+   <frame tileid="194" duration="200"/>
+   <frame tileid="256" duration="200"/>
+   <frame tileid="322" duration="200"/>
+   <frame tileid="384" duration="200"/>
+   <frame tileid="450" duration="200"/>
+   <frame tileid="512" duration="200"/>
+   <frame tileid="512" duration="200"/>
+   <frame tileid="450" duration="200"/>
+   <frame tileid="384" duration="200"/>
+   <frame tileid="322" duration="200"/>
+   <frame tileid="256" duration="200"/>
+   <frame tileid="194" duration="200"/>
+   <frame tileid="128" duration="200"/>
+   <frame tileid="66" duration="200"/>
+   <frame tileid="0" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="67" duration="200"/>
+   <frame tileid="129" duration="200"/>
+   <frame tileid="195" duration="200"/>
+   <frame tileid="257" duration="200"/>
+   <frame tileid="323" duration="200"/>
+   <frame tileid="385" duration="200"/>
+   <frame tileid="451" duration="200"/>
+   <frame tileid="513" duration="200"/>
+   <frame tileid="513" duration="200"/>
+   <frame tileid="451" duration="200"/>
+   <frame tileid="385" duration="200"/>
+   <frame tileid="323" duration="200"/>
+   <frame tileid="257" duration="200"/>
+   <frame tileid="195" duration="200"/>
+   <frame tileid="129" duration="200"/>
+   <frame tileid="67" duration="200"/>
+   <frame tileid="1" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="32">
+  <animation>
+   <frame tileid="32" duration="200"/>
+   <frame tileid="98" duration="200"/>
+   <frame tileid="160" duration="200"/>
+   <frame tileid="226" duration="200"/>
+   <frame tileid="288" duration="200"/>
+   <frame tileid="354" duration="200"/>
+   <frame tileid="416" duration="200"/>
+   <frame tileid="482" duration="200"/>
+   <frame tileid="544" duration="200"/>
+   <frame tileid="544" duration="200"/>
+   <frame tileid="482" duration="200"/>
+   <frame tileid="416" duration="200"/>
+   <frame tileid="354" duration="200"/>
+   <frame tileid="288" duration="200"/>
+   <frame tileid="226" duration="200"/>
+   <frame tileid="160" duration="200"/>
+   <frame tileid="98" duration="200"/>
+   <frame tileid="32" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="33">
+  <animation>
+   <frame tileid="33" duration="200"/>
+   <frame tileid="99" duration="200"/>
+   <frame tileid="161" duration="200"/>
+   <frame tileid="227" duration="200"/>
+   <frame tileid="289" duration="200"/>
+   <frame tileid="355" duration="200"/>
+   <frame tileid="417" duration="200"/>
+   <frame tileid="483" duration="200"/>
+   <frame tileid="545" duration="200"/>
+   <frame tileid="545" duration="200"/>
+   <frame tileid="483" duration="200"/>
+   <frame tileid="417" duration="200"/>
+   <frame tileid="355" duration="200"/>
+   <frame tileid="289" duration="200"/>
+   <frame tileid="227" duration="200"/>
+   <frame tileid="161" duration="200"/>
+   <frame tileid="99" duration="200"/>
+   <frame tileid="33" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
